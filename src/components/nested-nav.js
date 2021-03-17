cbcc384588dbfddea12de987b66ee677fc23e3e0
Jamie Gaehring <gaehj457@gmail.com>
2021-03-17 17:46:23 -0400

    Add a navigation drawer.

---
+++ src/components/nested-nav.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Link } from "gatsby-theme-material-ui";
+import { makeStyles } from '@material-ui/core/styles';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
+function NavListItem({ title, url }) {
+  return (
+    <ListItem button key={url}>
+      <Link to={url}>
+        <ListItemText primary={title}/>
+      </Link>
+    </ListItem>
+  );          
+}
+
+function NestedNavListItem({ title, children }) {
+  const [open, setOpen] = React.useState(false);
+  const handleClick = () => {
+    setOpen(!open);
+  };
+  return (
+    <React.Fragment>
+      <ListItem button onClick={handleClick}>
+        <ListItemText primary={title}/>
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        {children}
+      </Collapse>
+    </React.Fragment>
+  );
+}
+
+export default function NestedNav({ nav, header, top = true, ...rest }) {
+  const classes = useStyles();
+  let aria = null; let subheader = null;
+
+  if (header && top) {
+    aria = "nested-list-subheader";
+    subheader = (
+      <ListSubheader component="div" id="nested-list-subheader">
+        {header}
+      </ListSubheader>
+    );
+  }
+
+  return (
+    <List
+      component="nav"
+      aria-labelledby={aria}
+      subheader={subheader}
+      className={classes.root}
+      { ...rest }
+    >
+      {nav.map(({ title, url, children }) => {
+        if (typeof url === 'string') {
+          return <NavListItem title={title} url={url} key={title}/>;          
+        } else if (children.length > 0) {
+          return (
+            <NestedNavListItem title={title} key={title}>
+              <NestedNav nav={children} top={false} className={classes.nested}/>
+            </NestedNavListItem>
+          );
+        }
+        return null;
+      })}
+    </List>
+  );
+}
---
+++ src/components/responsive-drawer.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import NestedNav from './nested-nav';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+function ResponsiveDrawer(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const container = window.document.body;
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            <div className={classes.toolbar} />
+            <Divider />
+            <NestedNav nav={props.nav} header={props.header}/>
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            <div className={classes.toolbar} />
+            <Divider />
+            <NestedNav nav={props.nav} header={props.header}/>
+          </Drawer>
+        </Hidden>
+      </nav>
+    </div>
+  );
+}
+
+export default ResponsiveDrawer;
