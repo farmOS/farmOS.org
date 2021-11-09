1c0d2991abfe145e7cc7af0a56cf8b69f4c84648
Jamie Gaehring <gaehj457@gmail.com>
2021-11-09 06:45:55 -0500

    Move table of contents to DocsPage.

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import Seo from '../components/seo';
 import NestedNav from './nested-nav';
-import TableOfContents from './table-of-contents';
 import theme, { toolbarOffset } from '../theme';
 
 const contentWidth = 1280;
@@ -76,18 +75,6 @@ const useStyles = makeStyles({
     width: sidebarWidth,
     padding: theme.spacing(2),
   },
-  mainToC: {
-    ...toolbarOffset(({ minHeight }) => ({
-      top: minHeight,
-    })),
-    position: 'fixed',
-    left: sidebarOffset,
-    width: sidebarWidth,
-    [theme.breakpoints.down('md')]: {
-      left: `calc(50% + ${lineLengthInChars * 3 / 4}ch - ${contentWidth / 4}px)`,
-    },
-    padding: theme.spacing(2),
-  },
   footer: {
     backgroundColor: theme.palette.grey[900],
     color: theme.palette.grey[500],
@@ -127,22 +114,7 @@ export default function Layout({ children, location }) {
     };
     loadNav();
   }, []);
-  const [tocHtml, setTocHtml] = useState(post.tableOfContents);
   const title = post.headings.find(({ depth }) => depth === 1)?.value;
-  const toc = {
-    title,
-    __html: tocHtml,
-    headings: post.headings
-      .filter(({ depth }) => depth > 1)
-      .map(({ id }) => id),
-  };
-  useEffect(() => {
-    const div = document.createElement('div');
-    div.innerHTML = post.tableOfContents;
-    const ul1 = div.querySelector('ul li ul');
-    const ul2 = ul1 && ul1.outerHTML;
-    setTocHtml(ul2);
-  }, [post.tableOfContents]);
 
   const classes = useStyles();
   const [mobileOpen, setMobileOpen] = React.useState(false);
@@ -207,16 +179,7 @@ export default function Layout({ children, location }) {
           <Hidden mdDown implementation='css' className={classes.mainNav}>
             <NestedNav nav={nav} currentPathname={location.pathname}/>
           </Hidden>
-          <Box component='main'>
-            {children}
-          </Box>
-          {(
-            toc.__html
-            ? (<Hidden smDown implementation='css' className={classes.mainToC}>
-                <TableOfContents {...toc}/>
-              </Hidden>)
-            : null
-          )}
+          {children}
         </Container>
         <Box component='footer' className={classes.footer}>
           <Container className={classes.copyright}>
