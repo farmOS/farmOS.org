cbf4663c1af16fe4326a894c5da981bcbcfafdb1
Jamie Gaehring <gaehj457@gmail.com>
2021-11-09 07:13:00 -0500

    Redesign 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,53 +1,37 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link } from 'gatsby-material-ui-components';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Typography } from '@material-ui/core';
+import Seo from '../components/seo';
+import theme from "../theme";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+const useStyles = makeStyles({
+  main: {
+    '& h1': {
+      color: theme.palette.text.secondary,
+      fontWeight: 300,
+      fontSize: '2rem',
+      lineHeight: 1.3,
+      letterSpacing: '-.01em',
+      margin: '0 0 1.25rem',
+    },
+  },
+});
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
+  const classes = useStyles();
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <>
+      <Seo title="Not found"/>
+      <Box component='main' className={classes.main}>
+        <Typography variant='h1'>
+          404 - Not found
+        </Typography>
+        <Typography variant='p'>
+          <Link to="/">Return home</Link>.
+        </Typography>
+      </Box>
+    </>
   )
 }
 
