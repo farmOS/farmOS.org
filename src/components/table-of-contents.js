e68eebdfcda0ae824748a39c3e2ec93ba25767b0
Jamie Gaehring <gaehj457@gmail.com>
2021-11-11 15:50:33 -0500

    Fix active headings in table of contents.

--- src/components/table-of-contents.js
+++ src/components/table-of-contents.js
@@ -1,7 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import useActiveHeading from '../hooks/useActiveHeading';
 
 const useStyles = makeStyles(theme => ({
   title: {
@@ -33,57 +34,18 @@ const useStyles = makeStyles(theme => ({
   },  
 }));
 
-function findTopVisible(headingMap) {
-  const visible = [];
-  headingMap.forEach(({ h, a }, id) => {
-    const { top, left, bottom, right } = h.getBoundingClientRect();
-    const isVisible = (
-      top >= 0 &&
-      left >= 0 &&
-      bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-    if (isVisible) {
-      visible.push({ top, id, h, a });
-    }
-  });
-  visible.sort((a, b) => a.top - b.top);
-  return visible[0];
-}
-
 export default function TableOfContents(props) {
-  const { __html, headings, title = 'Table of contents' } = props;
-  const tocRef = useRef(null);
   const classes = useStyles();
+  const { __html, headings, title = 'Table of contents' } = props;
 
-  useEffect(() => {
-    const headingsMap = headings.reduce((map, id) => {
-      map.set(id, {
-        h: document.getElementById(id),
-        a: tocRef.current.querySelector(`a[href="#${id}"]`),
-      });
-      return map;
-    }, new Map());
-    let currentActiveHeading = headingsMap.get(headings[0]);
-    const setCurrentActiveHeading = () => {
-      const topVisible = findTopVisible(headingsMap);
-      if (!topVisible || topVisible.id === currentActiveHeading.id) {
-        return;
-      }
-      currentActiveHeading.a.classList.remove(classes.active);
-      currentActiveHeading = topVisible;
-      currentActiveHeading.a.classList.add(classes.active);
-    };
-    setCurrentActiveHeading();
-    const observer = new IntersectionObserver(setCurrentActiveHeading);
-    headingsMap.forEach(({ h }) => { observer.observe(h); });
-    return () => {
-      headingsMap.forEach(({ h }) => { observer.unobserve(h); });
-    }
-  }, [headings, tocRef, classes]);
+  const [tocRef, setActiveHeading] = useActiveHeading(headings, classes.active);
 
   return (<>
     <Typography component='label' className={classes.title}>{title}</Typography>
-    <Box ref={tocRef} className={classes.headings} dangerouslySetInnerHTML={{ __html }}/>
+    <Box
+      ref={tocRef}
+      className={classes.headings}
+      dangerouslySetInnerHTML={{ __html }}
+      onClick={setActiveHeading}/>
   </>);
 }
---
+++ src/hooks/useActiveHeading.js
@@ -0,0 +1,62 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+
+function computeVisibility(headingElement) {
+  const result = { bb: null, isVisible: false };
+  if (!(headingElement instanceof HTMLElement)) return result;
+  result.bb = headingElement.getBoundingClientRect();
+  const { top, left, bottom, right } = result.bb;
+  result.isVisible = top >= 0
+    && left >= 0
+    && bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    && right <= (window.innerWidth || document.documentElement.clientWidth);
+  return result;
+}
+
+function findTopVisible(headingMap) {
+  const visible = [];
+  headingMap.forEach((heading) => {
+    const { isVisible, bb } = computeVisibility(heading.h);
+    if (isVisible) visible.push({ ...heading, ...bb });
+  });
+  visible.sort((a, b) => a.top - b.top);
+  return visible[0];
+}
+
+export default function useActiveHeading(headings = [], activeClassName = '') {
+  const ref = useRef(null);
+  const [currentId, setCurrentId] = useState(headings[0]);
+  const headingsMap = useMemo(
+    () => new Map(headings.map(id => [id, { id }])),
+    [headings],
+  );
+  const setActiveHeading = useCallback(() => {
+    const topVisible = findTopVisible(headingsMap);
+    if (currentId && topVisible.id !== currentId) {
+      headingsMap.get(currentId).a.classList.remove(activeClassName);
+    }
+    if (topVisible && topVisible.id !== currentId) {
+      setCurrentId(topVisible.id);
+      topVisible.a.classList.add(activeClassName);
+    }
+  }, [activeClassName, currentId, headingsMap]);
+  const observer = useMemo(
+    () => new IntersectionObserver(setActiveHeading),
+    [setActiveHeading],
+  );
+  useEffect(() => {
+    const firstAnchor = ref && ref.current.querySelector(`a[href="#${currentId}"]`);
+    if (firstAnchor) {
+      firstAnchor.classList.add(activeClassName);
+    }
+    headingsMap.forEach((elements, id) => {
+      elements.h = document.getElementById(id);
+      elements.a = ref && ref.current.querySelector(`a[href="#${id}"]`);
+      observer.observe(elements.h);
+    });
+    return () => {
+      headingsMap.forEach(({ h }) => { observer.unobserve(h); });
+    };
+  }, [activeClassName, ref, currentId, headingsMap, observer]);
+
+  return [ref, () => setActiveHeading()];
+}
