d0130d623c881176249e298f59e034824942b20d
Jamie Gaehring <gaehj457@gmail.com>
2022-01-14 11:01:38 -0500

    Create blog index page.

---
+++ src/pages/blog.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { Link } from 'gatsby-material-ui-components';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Typography } from '@material-ui/core';
+import Seo from '../components/seo';
+import theme from '../theme';
+
+const useStyles = makeStyles({
+  main: {
+    '& h1': {
+      color: theme.palette.text.secondary,
+      fontWeight: 300,
+      fontSize: '2rem',
+      lineHeight: 1.3,
+      letterSpacing: '-.01em',
+      margin: '0 0 1.25rem',
+    },
+  },
+});
+
+const BlogIndex = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Seo title='farmOS | Blog'/>
+      <Box component='main' className={classes.main}>
+        <Typography variant='h1'>
+          Blog Page!
+        </Typography>
+        <Typography variant='body1'>
+          <Link to='/'>Return home</Link>.
+        </Typography>
+      </Box>
+    </>
+  );
+};
+
+export default BlogIndex
