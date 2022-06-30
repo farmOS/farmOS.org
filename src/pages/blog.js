227f2808b0ea2de034ea20611828f94b1f8f4c54
Jamie Gaehring <gaehj457@gmail.com>
2022-06-30 17:40:33 -0400

    First pass at styling main blog page.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,44 +6,70 @@ import Seo from '../components/seo';
 import theme from '../theme';
 import { graphql } from 'gatsby';
 
+const DEFAULT_AUTHOR = 'the farmOS Community';
+
 const useStyles = makeStyles({
   main: {
     '& h1': {
-      color: theme.palette.text.secondary,
       fontWeight: 300,
-      fontSize: '2rem',
+      fontSize: '3rem',
       lineHeight: 1.3,
       letterSpacing: '-.01em',
       margin: '0 0 1.25rem',
     },
   },
+  post: {
+    color: 'inherit',
+    textDecoration: 'none',
+    '& div': { paddingBottom: '2rem' },
+    '& h3': {
+      color: theme.palette.primary.light,
+      fontSize: '2rem',
+      lineHeight: 1.3,
+    },
+    '& h5': {
+      fontSize: '1rem',
+      lineHeight: 1.3,
+      '& span': {
+        fontWeight: 700,
+      },
+    },
+    '& p': {
+      color: theme.palette.text.hint,
+    },
+    '&:hover': {
+      textDecoration: 'none',
+      '& h3': { color: theme.palette.warning.main },
+    },
+  },
 });
 
 const BlogIndex = ({ data: { allMarkdownRemark } }) => {
+  const classes = useStyles();
   const posts = allMarkdownRemark.edges.map(({ node }, i) => {
     const {
       excerpt,
       fields: { pathname },
-      frontmatter: {title, date, },
+      frontmatter: { author = DEFAULT_AUTHOR, date, title, },
     } = node;
     return (
-    <Box component='a' href={pathname} key={i}>
-      <Typography variant='h3'>{title}</Typography>
-      <Typography variant='h5'>{date}</Typography>
-      <Typography variant='body1'>{excerpt}</Typography>
-    </Box>
+      <Link to={pathname} key={i} className={classes.post}>
+        <Box>
+          <Typography variant='h3'>{title}</Typography>
+          <Typography variant='h5'>
+            <span>{date}</span> by {author}
+          </Typography>
+          <Typography variant='body1'>{excerpt}</Typography>
+        </Box>
+      </Link>
     );
   });
-  const classes = useStyles();
   return (
     <>
       <Seo title='farmOS | Blog'/>
       <Box component='main' className={classes.main}>
         <Typography variant='h1'>
-          Blog Page!
-        </Typography>
-        <Typography variant='body1'>
-          <Link to='/'>Return home</Link>.
+          Community Blog
         </Typography>
         {posts}
       </Box>
